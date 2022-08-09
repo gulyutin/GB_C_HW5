6636d0634de170535d6ffa949e8f922054b3731c
Gulyutin <gulyutin@gmail.com>
2022-08-09 15:09:40 +0700

    Example 34 added

--- EX34/Program.cs
+++ EX34/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный 
+// случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество 
+// чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int [] array = new int[4];
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(99,999);
+    Console.Write(array[i] + " ");
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество чётных числе в массиве = {count}" );
